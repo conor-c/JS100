dbd4a214865d5b9cfad15028e89cb2b895780fd4
Conor Chung <ConorChung@gmail.com>
2020-11-03 11:07:27 -0800

    finished reading documentation 1&2

---
+++ reading_documentation/today.js
@@ -0,0 +1,4 @@
+let today = new Date();
+
+console.log(today.getYear()); //returns 2-3 digit years given the date according to local time (DEPRECATED)
+console.log(today.getFullYear()); //this returns 4 digits for 4 digit years of the given date according to local time
---
+++ reading_documentation/tweet.js
@@ -0,0 +1,6 @@
+let tweet = 'Woohoo! :-)';
+
+if (tweet.length > 140) {
+  console.log('Tweet is too long!');
+}
+//length is not a function, it's a property
